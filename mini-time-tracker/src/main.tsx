f68895f539e5361d2d0cd524ed07e1c0ac100e2b
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2025-06-22 15:49:01 +0200

    final commit

--- mini-time-tracker/src/main.tsx
+++ mini-time-tracker/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-import App from './routes'
+import App from './Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
